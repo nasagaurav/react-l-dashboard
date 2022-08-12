ac513f2b0c0437872cc33d9f40d6b41968fc6fbd
radhe shyam <radhe212221@gmail.com>
2022-08-12 08:21:41 +0530

    header completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 export default function Header() {
+  const navigate = useNavigate();
+  const logout = () => {
+    toast("logged out successfully")
+    navigate("/")
+  };
+
   return (
     <div>
       <>
@@ -17,31 +24,11 @@ export default function Header() {
             <i className="bi bi-list toggle-sidebar-btn" />
           </div>
           {/* End Logo */}
-          <div className="search-bar">
-            <form
-              className="search-form d-flex align-items-center"
-              method="POST"
-              action="#"
-            >
-              <input
-                type="text"
-                name="query"
-                placeholder="Search"
-                title="Enter search keyword"
-              />
-              <button type="submit" title="Search">
-                <i className="bi bi-search" />
-              </button>
-            </form>
-          </div>
+         
           {/* End Search Bar */}
           <nav className="header-nav ms-auto">
             <ul className="d-flex align-items-center">
-              <li className="nav-item d-block d-lg-none">
-                <a className="nav-link nav-icon search-bar-toggle " href="#">
-                  <i className="bi bi-search" />
-                </a>
-              </li>
+             
               {/* End Search Icon*/}
               <li className="nav-item dropdown">
                 <a
@@ -73,42 +60,7 @@ export default function Header() {
                       <p>30 min. ago</p>
                     </div>
                   </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li className="notification-item">
-                    <i className="bi bi-x-circle text-danger" />
-                    <div>
-                      <h4>Atque rerum nesciunt</h4>
-                      <p>Quae dolorem earum veritatis oditseno</p>
-                      <p>1 hr. ago</p>
-                    </div>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li className="notification-item">
-                    <i className="bi bi-check-circle text-success" />
-                    <div>
-                      <h4>Sit rerum fuga</h4>
-                      <p>Quae dolorem earum veritatis oditseno</p>
-                      <p>2 hrs. ago</p>
-                    </div>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li className="notification-item">
-                    <i className="bi bi-info-circle text-primary" />
-                    <div>
-                      <h4>Dicta reprehenderit</h4>
-                      <p>Quae dolorem earum veritatis oditseno</p>
-                      <p>4 hrs. ago</p>
-                    </div>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
+
                   <li className="dropdown-footer">
                     <a href="#">Show all notifications</a>
                   </li>
@@ -116,94 +68,7 @@ export default function Header() {
                 {/* End Notification Dropdown Items */}
               </li>
               {/* End Notification Nav */}
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link nav-icon"
-                  href="#"
-                  data-bs-toggle="dropdown"
-                >
-                  <i className="bi bi-chat-left-text" />
-                  <span className="badge bg-success badge-number">3</span>
-                </a>
-                {/* End Messages Icon */}
-                <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
-                  <li className="dropdown-header">
-                    You have 3 new messages
-                    <a href="#">
-                      <span className="badge rounded-pill bg-primary p-2 ms-2">
-                        View all
-                      </span>
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li className="message-item">
-                    <a href="#">
-                      <img
-                        src="assets/img/messages-1.jpg"
-                        alt=""
-                        className="rounded-circle"
-                      />
-                      <div>
-                        <h4>Maria Hudson</h4>
-                        <p>
-                          Velit asperiores et ducimus soluta repudiandae labore
-                          officia est ut...
-                        </p>
-                        <p>4 hrs. ago</p>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li className="message-item">
-                    <a href="#">
-                      <img
-                        src="assets/img/messages-2.jpg"
-                        alt=""
-                        className="rounded-circle"
-                      />
-                      <div>
-                        <h4>Anna Nelson</h4>
-                        <p>
-                          Velit asperiores et ducimus soluta repudiandae labore
-                          officia est ut...
-                        </p>
-                        <p>6 hrs. ago</p>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li className="message-item">
-                    <a href="#">
-                      <img
-                        src="assets/img/messages-3.jpg"
-                        alt=""
-                        className="rounded-circle"
-                      />
-                      <div>
-                        <h4>David Muldon</h4>
-                        <p>
-                          Velit asperiores et ducimus soluta repudiandae labore
-                          officia est ut...
-                        </p>
-                        <p>8 hrs. ago</p>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li className="dropdown-footer">
-                    <a href="#">Show all messages</a>
-                  </li>
-                </ul>
-                {/* End Messages Dropdown Items */}
-              </li>
+
               {/* End Messages Nav */}
               <li className="nav-item dropdown pe-3">
                 <a
@@ -217,23 +82,20 @@ export default function Header() {
                     className="rounded-circle"
                   />
                   <span className="d-none d-md-block dropdown-toggle ps-2">
-                    K. Anderson
+                    admin
                   </span>
                 </a>
                 {/* End Profile Iamge Icon */}
                 <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                   <li className="dropdown-header">
-                    <h6>Kevin Anderson</h6>
-                    <span>Web Designer</span>
+                    <h6>Gaurav Nasa</h6>
+                    <span>Senior Software Engineer</span>
                   </li>
                   <li>
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                    <a
-                      className="dropdown-item d-flex align-items-center"
-                      href="users-profile.html"
-                    >
+                    <a className="dropdown-item d-flex align-items-center">
                       <i className="bi bi-person" />
                       <span>My Profile</span>
                     </a>
@@ -242,10 +104,7 @@ export default function Header() {
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                    <a
-                      className="dropdown-item d-flex align-items-center"
-                      href="users-profile.html"
-                    >
+                    <a className="dropdown-item d-flex align-items-center">
                       <i className="bi bi-gear" />
                       <span>Account Settings</span>
                     </a>
@@ -254,10 +113,7 @@ export default function Header() {
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                    <a
-                      className="dropdown-item d-flex align-items-center"
-                      href="pages-faq.html"
-                    >
+                    <a className="dropdown-item d-flex align-items-center">
                       <i className="bi bi-question-circle" />
                       <span>Need Help?</span>
                     </a>
@@ -267,8 +123,8 @@ export default function Header() {
                   </li>
                   <li>
                     <a
+                      onClick={logout}
                       className="dropdown-item d-flex align-items-center"
-                      href="#"
                     >
                       <i className="bi bi-box-arrow-right" />
                       <span>Sign Out</span>
