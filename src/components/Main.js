4f4c15ca62c7e2d75d9657d0176034766684b95e
radhe shyam <radhe212221@gmail.com>
2022-08-12 08:27:14 +0530

    main completed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,47 +1,26 @@
 import React from "react";
 
-export default function Main() {
+export default function Main(props) {
   return (
     <div>
       <>
         <main id="main" className="main">
           <div className="pagetitle">
-            <h1>Blank Page</h1>
+            <h1>Dashboard-Page</h1>
             <nav>
               <ol className="breadcrumb">
                 <li className="breadcrumb-item">
-                  <a href="index.html">Home</a>
+                  <a href="index.html">b1</a>
                 </li>
-                <li className="breadcrumb-item">Pages</li>
-                <li className="breadcrumb-item active">Blank</li>
+                <li className="breadcrumb-item">b2</li>
+                <li className="breadcrumb-item active">b3</li>
               </ol>
             </nav>
           </div>
           {/* End Page Title */}
           <section className="section">
             <div className="row">
-              <div className="col-lg-6">
-                <div className="card">
-                  <div className="card-body">
-                    <h5 className="card-title">Example Card</h5>
-                    <p>
-                      This is an examle page with no contrnt. You can use it as
-                      a starter for your custom pages.
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-6">
-                <div className="card">
-                  <div className="card-body">
-                    <h5 className="card-title">Example Card</h5>
-                    <p>
-                      This is an examle page with no contrnt. You can use it as
-                      a starter for your custom pages.
-                    </p>
-                  </div>
-                </div>
-              </div>
+              {props.children}
             </div>
           </section>
         </main>
