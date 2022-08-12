11078d05ebf13a0997085d87483ede1d203ea5c8
radhe shyam <radhe212221@gmail.com>
2022-08-12 08:14:12 +0530

    login logic added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import App from "./App";
+import 'react-toastify/dist/ReactToastify.css';
 
-import App from './App';
-
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
     <App />
+    <ToastContainer />
   </StrictMode>
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,108 @@
-import React from 'react'
-
+import React, { useRef } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
+  const r1 = useRef();
+  const r2 = useRef();
+  const navigate = useNavigate();
+  const onSubmit = () => {
+    const username = r1.current.value;
+    const password = r2.current.value;
+    if (username === "admin" && password === "admin") {
+      toast("loggedin successfully");
+      navigate("/Dashboard");
+    } else {
+      toast("failed to login try again");
+    }
+  };
   return (
-    <div>Login</div>
-  )
+    <div>
+      <main>
+        <div className="container">
+          <section className="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+                  <div className="d-flex justify-content-center py-4">
+                    <a
+                      href="index.html"
+                      className="logo d-flex align-items-center w-auto"
+                    ></a>
+                  </div>
+                  {/* End Logo */}
+                  <div className="card mb-3">
+                    <div className="card-body">
+                      <div className="pt-4 pb-2">
+                        <h5 className="card-title text-center pb-0 fs-4">
+                          Login to Your Account
+                        </h5>
+                        <p className="text-center small">
+                          Enter your username &amp; password to login
+                        </p>
+                      </div>
+                      <div className="row g-3 needs-validation" noValidate="">
+                        <div className="col-12">
+                          <label htmlFor="yourUsername" className="form-label">
+                            Username
+                          </label>
+                          <div className="input-group has-validation">
+                            <span
+                              className="input-group-text"
+                              id="inputGroupPrepend"
+                            >
+                              @
+                            </span>
+                            <input
+                              type="text"
+                              name="username"
+                              className="form-control"
+                              id="yourUsername"
+                              required=""
+                              ref={r1}
+                            />
+                            <div className="invalid-feedback">
+                              Please enter your username.
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-12">
+                          <label htmlFor="yourPassword" className="form-label">
+                            Password
+                          </label>
+                          <input
+                            type="password"
+                            name="password"
+                            className="form-control"
+                            id="yourPassword"
+                            required=""
+                            ref={r2}
+                          />
+                          <div className="invalid-feedback">
+                            Please enter your password!
+                          </div>
+                        </div>
+
+                        <div className="col-12">
+                          <button
+                            className="btn btn-primary w-100"
+                            type="submit"
+                            onClick={onSubmit}
+                          >
+                            Login
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="credits">
+                    Designed by <a href="#">Demo</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
 }
